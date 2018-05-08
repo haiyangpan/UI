931c7788f55fa97dc3f3598a0607b12a32801808
haiyangpan <598441578@qq.com>
2018-05-08 17:12:03 +0800

    增加卡片阴影、轮播，局部微调

--- js/index.js
+++ js/index.js
@@ -1,3 +1,42 @@
+///////////////
+// 我要学card轮播 //
+///////////////
+$(function(){
+	var index = 0;
+	$('.swBtn').on("click",function(){
+		allHidden();
+		if($(this).index() == 0){
+			index--;
+			if(index < -2){
+				index = 0;
+			}
+			activeFun(Math.abs(index));
+		}
+		if($(this).index() == 4){
+			if(index >= 2){
+				index = -1;
+			}
+			index++;
+			activeFun(Math.abs(index));
+		}
+	})
+	$('.bar').hover(function(){
+		allHidden();
+		activeFun($(this).index());
+	})
+	
+	function allHidden(){
+		for (var i = 0; i < $('.showImg').length; i++) {
+			$('.showImg').eq(i).css("display","none");
+			$('.bar').eq(i).removeClass("active");
+		}
+	}
+	function activeFun(index){
+		$('.showImg')[index].style.display="block";
+		$('.bar').eq(index).addClass("active");
+	}
+})
+
 // 讲师资料
 $(".teaBar>ul>li").hover(function(){
 	for (var i = 0; i < $(".teaBar>ul>li").length; i++) {
